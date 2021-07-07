615bb8a86461076c4ecaeedcd19de2f40ece2a2c
ng169 <pokeakshay123@gmail.com>
2021-07-07 14:15:17 +0530

    changes in routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+import Pokedex from './components/Pokedex';
+import Pokemon from './components/Pokemon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route exact path="/PokeDex/" render={(props) => <Pokedex {...props} />} />
+      <Route exact path="/PokeDex/:pokemonId" render={(props) => <Pokemon {...props} />} />
+    </Switch>
+
+  )
 }
 
 export default App;
